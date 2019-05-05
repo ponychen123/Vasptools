ab2d6d704b155de2dc11b77c923c4adc97dd6d8e
ponychen <18709821294@outlook.com>
2019-05-05 10:03:32 +0800

    update MSnebmake.pl

--- MSnebmaker.pl
+++ MSnebmaker.pl
@@ -1,4 +1,5 @@
 #!perl
+#add a sortatoms function ,sort the atoms if the atoms oder changes . but not works for stacking faults!!! ponychen 20190502.
 #i like model systems in MS but it's awful to make neb path by using nebmake.pl under linux. sometimes you need further modulate the path.thus i
 #wrote this script to create neb path in the MS. make life better.....
 #usage: 1.drag your initial and final structure into MS
@@ -12,10 +13,11 @@ use Getopt::Long;
 use MaterialsScript qw(:all);
 
 #chage following values
-my $inifile = "0"; #your initial xsd filename
-my $finfile = "8"; #your final xsd filename
-my $images_num = 7; #how much images would you create
+my $inifile = "00"; #your initial xsd filename
+my $finfile = "08"; #your final xsd filename
+my $images_num = 5; #how much images would you create
 my $dmin = 2.0;       #belowe which the atoms are thought to be close, unit angstrom
+my $sort = "False"; # if True, sored the final structure by the initial structure
 
 #do not change below codes unless you konw what yuou are doing!!!
 my @stepx;
@@ -43,6 +45,34 @@ sub check_d {
                 $b->Append(sprintf " caution! the %sth atom and %sth atom in %s.xsd are too close. \n", $m, $n, $c);
                 }}}
                 }
+
+sub sortatoms {
+    my ($k, $l) = @_;
+    my $sorteddoc = Documents->New("sorted.xsd");
+    $sorteddoc->CopyFrom($inidoc);
+    my $sortedatoms = $sorteddoc->UnitCell->Atoms;
+    for(my $i=0; $i<$sortedatoms->Count; ++$i){
+        my $rc = 5;
+        my $atomnew = @$l[0];
+        foreach my $atoml (@$l){
+            my $r = (@$k[$i]->X - $atoml->X)**2 + (@$k[$i]->Y - $atoml->Y)**2 + (@$k[$i]->Z - $atoml->Z)**2;
+            if ($r < $rc and @$k[$i]->ElementSymbol == $atoml->ElementSymbol){
+                $rc = $r;
+                $atomnew = $atoml;
+                }
+        }
+        @$sortedatoms[$i]->X = $atomnew->X;
+        @$sortedatoms[$i]->Y = $atomnew->Y;
+        @$sortedatoms[$i]->Z = $atomnew->Z;
+        
+        }}
+        
+if ( $sort eq "True" ){
+
+sortatoms($iniatoms, $finatoms);
+$finatoms = $Documents{"sorted.xsd"}->UnitCell->Atoms;
+$finfile = "sorted";
+       }
 
 check_d($iniatoms, $status, $inifile);
 check_d($finatoms, $status, $finfile);
