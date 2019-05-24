dcf46708b59038de7afefbc9a6c502545b8d9fb1
ponychen <18709821294@outlook.com>
2019-05-24 15:28:24 +0800

    update drms.sh, add pbc condition

--- drms.sh
+++ drms.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#add pbc boundary condition, but now only surpport Direct format POSCAR
 #this script tells you the root mean squre bonds length
 #Usage: dist.sh inputfile
 #2019/04/25 pony chen
@@ -19,21 +20,25 @@ else
 fi
 
 if [ $dire -eq 0 ];then
-
-
 	     awk -v begin=$begin -v end=$end -v r0=$r0 '
-		   NR>=3 && NR<=5 {r[NR]=$1;s[NR]=$2;t[NR]=$3}
+		   NR==2 {scal=$1}
+		   NR>=3 && NR<=5 {r[NR]=$1*scal;s[NR]=$2*scal;t[NR]=$3*scal}
 		   NR>=begin && NR<=end {x0[NR]=$1;y0[NR]=$2;z0[NR]=$3}
-	       END{for(i=begin;i<=end;i=i+1){
-		        x1[i]=x0[i]*r[3] + y0[i]*r[4] + z0[i]*r[5];
-				y1[i]=x0[i]*s[3] + y0[i]*s[4] + z0[i]*s[5];
-				z1[i]=x0[i]*t[3] + y0[i]*t[4] + z0[i]*t[5];
-		    	};
-				u=0;
+	       END{	u=0;
 				v=0;
                for(j=begin;j<end;j=j+1){
 				   for(k=j+1;k<=end;k=k+1){
-					   m=(x1[k]-x1[j])^2 + (y1[k]-y1[j])^2 + (z1[k]-z1[j])^2;
+				       veca=x0[j]-x0[k];vecb=y0[j]-y0[k];vecc=z0[j]-z0[k]
+                       if(veca>0.5){veca-=1}
+					   if(veca<-0.5){veca+=1}
+					   if(vecb>0.5){vecb-=1}
+					   if(vecb<-0.5){vecb+=1}
+					   if(vecc>0.5){vecc-=1}
+					   if(vecc<-0.5){vecc+=1}
+					   x = veca*r[3]+vecb*r[4]+vecc*r[5]
+					   y = veca*s[3]+vecb*s[4]+vecc*s[5]
+					   z = veca*t[3]+vecb*t[4]+vecc*t[5]
+					   m = x**2 + y**2 + z**2
 					   if (sqrt(m) < r0 ){
                            u=u + m;
 						   v=v + 1;
@@ -44,22 +49,7 @@ if [ $dire -eq 0 ];then
 			   printf("the rms bond distance is %f \n", q);
 		}' $1 
 else
-	     awk -v begin=$begin -v end=$end -v r0=$r0 '
-		         NR>=begin && NR<=end {x1[NR]=$1;y1[NR]=$2;z1[NR]=$3}
-			     END{u=0;
-				     v=0;
-				     for(j=begin;j<end;j=j+1){
-				         for(k=j+1;k<=end;k=k+1){
-					        m=(x1[k]-x1[j])^2 + (y1[k]-y1[j])^2 + (z1[k]-z1[j])^2;
-							if (sqrt(m) < r0 ){
-						          u=u + m ;
-								  v=v + 1 ;
-							  };
-				         };
-			            };
-						q=sqrt(u/v);
-						printf("the rms bond distance is %f \n", q);
-				 }' $1 ;
+	echo "please make sure your POSCAR was in direct dormat"    
 fi
 
 
