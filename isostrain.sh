af22c637c6c753eddd9a76cde6cf470471385e14
ponychen123 <18709821294@outlook.com>
2019-10-29 15:50:42 +0800

    update isostrain.sh

--- isostrain.sh
+++ isostrain.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 #ponychen
 
+mode=1
 ini_strain=1.0
 fin_strain=1.5
 num=20
 
 step=`echo "scale=2;($fin_strain-$ini_strain)/($num-1)"|bc -l`
+
 for i in $(seq $ini_strain $step $fin_strain)
 do  
-	if [ ! -d $i ];then
-	    mkdir $i
+	if [ $mode == 1 ];then
+	    if [ ! -d $i ];then
+	        mkdir $i
+	    else
+		    rm -r $i/*
+	    fi
+	    cp INCAR KPOINTS POTCAR POSCAR run_vasp.sh ./$i
+	    sed -in "2c $i" $i/POSCAR
+	    rm $i/POSCARn
 	else
-		rm -r $i/*
+		E=`grep "E0=" $i/OSZICAR |tail -1 | awk '{printf("%12.9f", $5)}'`
+		echo $i $E >> avsE.txt
 	fi
-	cp INCAR KPOINTS POTCAR POSCAR run_vasp.sh ./$i
-	sed -in "2c $i" $i/POSCAR
-	rm $i/POSCARn
 done
 
 echo "done!"
