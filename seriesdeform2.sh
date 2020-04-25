3b156cccc678e63015cd515d7f262fbfb71af35c
ponychen123 <18709821294@outlook.com>
2020-04-25 17:03:00 +0800

    add seriesdeforem2.sh

--- seriesdeform2.sh
+++ seriesdeform2.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-#20190528 fix a bug in prefactor of POSCAR
-#20190524 fix a bug
-#a simple bash shell to add a series of specific strain on the Direct coordination
+#a simple bash shell to add a series of specific strain on the Cartesian coordination
 #warning: you should use the direct coordition 
 #warning: your cell should better to be orthogonallty
-#ponychen 2019/05/23
+#ponychen 2020/04/25
 
 orientation="XY" #set which type of strain
 initial=0.0      #set the initial strain
@@ -25,15 +23,15 @@ eval $(awk -v arr1="${newstrain[*]}" '
 			     adds[5]/=2;
 			     adds[6]/=2;}
 		   NR>=3 && NR<=5 {r0[NR]=$1;s0[NR]=$2;t0[NR]=$3}
-		   END{r1[3]=adds[1]*r0[3]+adds[4]*r0[4]+adds[5]*r0[5];
-		       s1[3]=adds[1]*s0[3]+adds[4]*s0[4]+adds[5]*s0[5];
-			   t1[3]=adds[1]*t0[3]+adds[4]*t0[4]+adds[5]*t0[5];
-			   r1[4]=adds[4]*r0[3]+adds[2]*r0[4]+adds[6]*r0[5];
-			   s1[4]=adds[4]*s0[3]+adds[2]*s0[4]+adds[6]*s0[5];
-			   t1[4]=adds[4]*t0[3]+adds[2]*t0[4]+adds[6]*t0[5];
-			   r1[5]=adds[5]*r0[3]+adds[6]*r0[4]+adds[3]*r0[5];
-			   s1[5]=adds[5]*s0[3]+adds[6]*s0[4]+adds[3]*s0[5];
-			   t1[5]=adds[5]*t0[3]+adds[6]*t0[4]+adds[3]*t0[5];
+		   END{r1[3]=r0[3]*adds[1]+s0[3]*adds[4]+t0[3]*adds[5];
+		       s1[3]=r0[3]*adds[4]+s0[3]*adds[2]+t0[3]*adds[6];
+			   t1[3]=r0[3]*adds[5]+s0[3]*adds[6]+t0[3]*adds[3];
+			   r1[4]=r0[4]*adds[1]+s0[4]*adds[4]+t0[4]*adds[5];
+			   s1[4]=r0[4]*adds[4]+s0[4]*adds[2]+t0[4]*adds[6];
+			   t1[4]=r0[4]*adds[5]+s0[4]*adds[6]+t0[4]*adds[3];
+			   r1[5]=r0[5]*adds[1]+s0[5]*adds[4]+t0[5]*adds[5];
+			   s1[5]=r0[5]*adds[4]+s0[5]*adds[2]+t0[5]*adds[6];
+			   t1[5]=r0[5]*adds[5]+s0[5]*adds[6]+t0[5]*adds[3];
 			   for(i=3;i<=5;i++){
 				   printf("newpos[%s]=\" %9.6f %9.6f %9.6f\n\";",i,r1[i],s1[i],t1[i])}
 			   }' POSCAR)
